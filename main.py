72f1c4414cb9a2fe942c0dcc068c97baf8aa7033
Rahul-neetha <121078467+Rahul-neetha@users.noreply.github.com>
2024-08-06 01:02:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from langchain.llms import OpenAI
 import streamlit as st
 
-# openai_key = 'sk-0I2GNbJuKedu2K6X1xSp8TxCbeafpDB9YOl3Fu1a30T3BlbkFJYilq0dtLy0-L2FGMCuGfgmPruOLxA9FPJRYpYawf0A'
+
 openai_key = st.secrets["OPENAI_API_KEY"] 
 # initialize st framework
 os.environ["OPENAI_API_KEY"]=openai_key
